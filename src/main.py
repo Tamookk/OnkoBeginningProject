22bb08b3d3cf2940dc5d9adffa2e13eedf49eb58
Joshua <jjs910@hotmail.com>
2021-05-13 16:46:57 +1000

    Add initial project files

---
+++ src/main.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    print("Hello, world!")
+
+# TODO: Access user home directory (Windows, MacOS, Linux)
+#       Access hidden application directory (create if not present)
+#       Implement UI wireframes
+#       Create unit tests for above functionality
